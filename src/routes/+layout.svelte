822d4404816c969f3fb9a9ad477e068b8f8d8753
daveripper0020 <daveripper0020@gmail.com>
2025-01-06 17:38:47 +0900

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import "../app.css";
+    let { children } = $props();
+</script>
+
+<main class="flex p-4 items-center justify-center w-full h-full">
+    {@render children()}
+</main>
---
+++ src/routes/links/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+</script>
+
+<div class="bg-cover absolute -top-20 -left-20 w-[200vw] h-[200vh] overflow-hidden"
+    style="background-image: url(/images/links/paper.webp);">
+
+</div>
