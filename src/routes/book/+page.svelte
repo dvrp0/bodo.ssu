c611ca36091ddc0c709dcec94157339444e73f33
daveripper0020 <daveripper0020@gmail.com>
2025-01-15 01:35:36 +0900

    지금은1월15일오전1시35분이제그만놓아주고자한다

---
+++ src/routes/book/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { beforeNavigate, goto } from "$app/navigation";
+    import { PUBLIC_BOOK_LINK } from "$env/static/public";
+    import { MetaTags } from "svelte-meta-tags";
+
+    const name = "숭실대학교 극예술연구회 제103회 정기공연 「보도지침」";
+
+    beforeNavigate(() => {
+        goto(PUBLIC_BOOK_LINK);
+    })
+</script>
+
+<MetaTags
+    title={name}
+    openGraph={{
+        url: "https://bodo-ssu.pages.dev/book",
+        title: name,
+        images: [
+            {
+                url: "https://bodo-ssu.pages.dev/images/links/og-thumbnail.webp",
+                width: 1200,
+                height: 627,
+                alt: name
+            }
+        ],
+        siteName: name
+    }}
+/>
--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -1,8 +1,35 @@
 <script lang="ts">
+    import {
+        PUBLIC_BODO_INSTAGRAM,
+        PUBLIC_BODO_PAMPHLET,
+        PUBLIC_PLAY_INSTAGRAM,
+        PUBLIC_PLAY_YOUTUBE
+    } from "$env/static/public";
 
+    let screenHeight = $state(0);
+    $effect(() => {
+        screenHeight = window.innerHeight;
+        console.log(screenHeight);
+    });
 </script>
 
-<div class="bg-cover absolute -top-20 -left-20 w-[200vw] h-[200vh] overflow-hidden"
-    style="background-image: url(/images/links/paper.webp);">
-
+<!-- <div class="bg-cover bg-center bg-no-repeat w-full h-[120vh] absolute top-0 z-10 pointer-events-none" style="background-image: url(/images/links/noise.webp);"></div>
+<div class="bg-cover bg-center bg-no-repeat w-full h-[120vh] absolute top-0 mix-blend-multiply pointer-events-none" style="background-image: url(/images/links/crumple.webp);"></div>
+<div class="bg-cover bg-center bg-no-repeat w-full h-[120vh] absolute top-0 -z-10 pointer-events-none" style="background-image: url(/images/links/paper.webp);"></div> -->
+<div class="w-full h-full pt-20 py-10 flex flex-col items-center" style="--height: {screenHeight}px;">
+    <img class="py-10" src="/images/links/logo.webp" />
+    <div class="flex flex-col justify-center items-center pt-10 gap-4">
+        <a href={PUBLIC_BODO_INSTAGRAM} target="_blank">
+            <img src="/images/links/bodo-insta.webp" />
+        </a>
+        <a href={PUBLIC_BODO_PAMPHLET} target="_blank">
+            <img src="/images/links/bodo-pam.webp" />
+        </a>
+        <a href={PUBLIC_PLAY_INSTAGRAM} target="_blank">
+            <img src="/images/links/play-insta.webp" />
+        </a>
+        <a href={PUBLIC_PLAY_YOUTUBE} target="_blank">
+            <img src="/images/links/play-youtube.webp" />
+        </a>
+    </div>
 </div>
