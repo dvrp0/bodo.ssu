e3e5e4d00e14d4ea20d7d4fe961957128ffcf665
daveripper0020 <daveripper0020@gmail.com>
2025-01-15 01:53:57 +0900

    1시53분이고팜플렛을추가했어요

--- src/routes/pamphlet/+page.svelte
+++ src/routes/pamphlet/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
-
+    const pages = Object.keys(import.meta.glob("/static/images/pamphlet/*.*", {
+        eager: true
+    }))
+        .map(x => x.replace("/static", ""))
+        .sort((a, b) => a.localeCompare(b, undefined, { numeric: true }));
+    
 </script>
 
-hi!
+<div class="grid grid-cols-1 mt-4 gap-4">
+    {#each pages as src}
+        <img {src} loading="lazy" />
+    {/each}
+</div>
